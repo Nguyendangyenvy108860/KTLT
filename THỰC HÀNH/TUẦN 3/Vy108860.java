d671519e856c20fe8e85b11f13932f4ac9791bd3
Nguyendangyenvy108860 <vynguyenyen127@gmail.com>
2024-10-22 09:18:55 +0700

    Add files via upload

---
+++ THỰC HÀNH/TUẦN 3/Vy108860.java
@@ -0,0 +1,38 @@
+package HoTenSinhVien.src.src.buoi3;
+
+import java.util.Scanner;
+
+public class Vy108860 {
+    public static void main(String[] args)   {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ho va ten: ");
+        scanner.nextLine();
+        System.out.print("Ma sinh vien: ");
+        scanner.nextShort();
+        System.out.print("Ngay sinh: ");
+        scanner.nextLong();
+        System.out.print("Tuoi: ");
+        scanner.nextInt();
+        System.out.print("Gioi tinh: ");
+        scanner.nextBoolean();
+        System.out.print("Lop: ");
+        scanner.nextLine();
+        System.out.print("Khoa: ");
+        scanner.nextLine();
+        System.out.print("Dia chi: ");
+        scanner.nextLine();
+        System.out.print("-------------");
+        System.out.print("Ho va ten: ");
+        System.out.print("'Ma sinh vien: ");
+        System.out.print("Ngay sinh: ");
+        System.out.print("Tuoi: ");
+        System.out.print("Gioi tinh: ");
+        System.out.print("Lop: ");
+        System.out.print("Khoa: ");
+        System.out.print("Dia chi: ");
+
+
+
+
+    }
+}
