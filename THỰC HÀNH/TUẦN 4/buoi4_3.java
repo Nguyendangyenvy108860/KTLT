e120c2167c229cb1ade792f7e9959534c6abe670
Nguyendangyenvy108860 <vynguyenyen127@gmail.com>
2024-10-27 09:37:48 +0700

    Add files via upload

---
+++ THỰC HÀNH/TUẦN 4/buoi4_3.java
@@ -0,0 +1,14 @@
+public class buoi4_3 {
+    public static void main(String[] args) {bai4_2();}
+    public static void bai4_2() {
+         String nguHanh [] ={"kim", "moc", "thuy", "hoa", "tho"};
+         for(short i=0;i<nguHanh.length;i++) {
+         System.out.print("\nDanh sach phan tu cua mang la: " + nguHanh[i]);
+         }
+        }
+    }
+    
+
+
+    
+
