93d84eb6b039f345212a13662d3e31c039ad32fc
Nguyendangyenvy108860 <vynguyenyen127@gmail.com>
2024-11-02 20:10:23 +0700

    Add files via upload

---
+++ THỰC HÀNH/TUẦN 4/bai4_3.java
@@ -0,0 +1,17 @@
+public class bai4_3 {
+    public static void main(String[] args) {bai4_3();}
+    public static void bai4_3() {
+    {
+        int arr1[][] = new int[2][3];
+        System.out.println("\nDanh sách phần tử của mảng arr1 là: ");
+        for(int i=0; i<arr1.length; i++)
+        {
+            for(int j=0; j<arr1[i].length; j++)
+            {
+                System.out.printf("arr1[%d][%d]",i,j);
+            }
+            System.out.println();
+        }
+    }
+    }
+}
